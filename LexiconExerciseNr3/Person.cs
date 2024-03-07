e0b70e1ff4ba4eecdfd1ed9d2afc2c2495b95797
Eugenia <musat.eugenia@gmail.com>
2024-03-07 21:39:22 +0100

    3.1.1 Encapsulation
    Create new class Person
    Add private fields and public properties
    Create try catch block in Program

---
+++ LexiconExerciseNr3/Person.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconExerciseNr3
+{
+    internal class Person
+    {
+
+        private int age;
+        private string fName;
+        private string lName;
+        private int height;
+        private int weight;
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Negative input not allowed");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+        public string FName
+        {
+            get { return fName; }
+            set
+            {
+                if (value.Length < 2 || value.Length > 10)
+                {
+                    throw new ArgumentException("Length's first name must be between 2 and 10 characters");
+                }
+                else
+                {
+                    fName = value;
+                }
+            }
+        }
+        public string LName
+        {
+            get { return lName; }
+            set
+            {
+                if (value.Length < 3 || value.Length > 15)
+                {
+                    throw new ArgumentException("Length's last name must be between 3 and 15 characters");
+                }
+                else
+                {
+                    lName = value;
+                }
+            }
+        }
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public int Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+    }
+}
--- LexiconExerciseNr3/Program.cs
+++ LexiconExerciseNr3/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person person = new Person();
+
+            try
+            {
+                Console.WriteLine("Add age.");
+                person.Age = int.Parse(Console.ReadLine());
+                Console.WriteLine("Add a first name.");
+                person.FName = Console.ReadLine().ToLower();
+                Console.WriteLine("Add a last name.");
+                person.LName = Console.ReadLine().ToLower();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
         }
     }
 }
