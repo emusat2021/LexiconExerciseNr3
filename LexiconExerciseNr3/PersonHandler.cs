844bf989ad0979cceaf4faf3ffc3128444545ed4
Eugenia <musat.eugenia@gmail.com>
2024-03-07 21:45:59 +0100

    3.1.2 Add PersonHandler class

---
+++ LexiconExerciseNr3/PersonHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconExerciseNr3
+{
+    internal class PersonHandler
+    {
+        public void SetAge(Person pers, int age)
+        {
+            pers.Age = age;
+        }
+
+    }
+}
